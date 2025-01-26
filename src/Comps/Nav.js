b462d1d4d91634524229feb4c64be0fecb948bc0
Aguadebebe <81484212+Aguadebebe@users.noreply.github.com>
2025-01-26 12:12:31 -0500

    Intial commit

--- src/Comps/Nav.js
+++ src/Comps/Nav.js
@@ -3,7 +3,7 @@
 const Nav = ({ handleRightFlip, handleLeftFlip }) => {
     return (
         <div>
-          <button onClick={handleRightFlip} style={{ marginRight: ".1em"}}>FLip R</button>
+          <button onClick={handleRightFlip} style={{ marginRight: ".1em", color: "red"}}>FLip R</button>
           <button onClick={handleLeftFlip}>Flip L</button>
         </div>
        
